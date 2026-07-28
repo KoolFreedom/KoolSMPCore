067250c3cdb8b4face647532e7a0d17b4abef716
Axdmaz <h98579694@gmail.com>
2026-07-28 08:14:45 +0100

    Add imports for KoolCommand and CommandParameters

--- src/main/java/eu/koolfreedom/command/impl/LaunchCommand.java
+++ src/main/java/eu/koolfreedom/command/impl/LaunchCommand.java
@@ -1,5 +1,7 @@
 package eu.koolfreedom.command.impl;
 
+import eu.koolfreedom.command.KoolCommand;
+import eu.koolfreedom.command.annotation.CommandParameters;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
