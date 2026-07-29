a16e22152765e01617ab0d7433f740d72e6eaaab
Axdmaz <h98579694@gmail.com>
2026-07-29 16:50:01 +0100

    Add kfchelp command for KoolSmpCore help
    f

---
+++ src/main/java/eu/koolfreedom/command/impl/kfchelp.java
@@ -0,0 +1,20 @@
+package eu.koolfreedom.command.impl;
+
+import eu.koolfreedom.command.KoolCommand;
+import eu.koolfreedom.command.annotation.CommandParameters;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandParameters(name = "kfchelp", description = "Shows help information for KoolSmpCore",  aliases = {"feedall"})
+
+public class SatisfyAllCommand extends KoolCommand {
+
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args) {
+        msg(sender, "<red>If you need help setting up KoolSmpCore, please visit our discord here: https://discord.gg/MTYrSgVkmd");
+        msg(sender, "<red>Or run /help KoolSmpCore");
+        
+        return true;
+    }
+}
