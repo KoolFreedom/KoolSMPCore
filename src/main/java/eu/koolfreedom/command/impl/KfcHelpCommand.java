3e0c4211ead6d956653bd57cb53c4d59585cf962
Axdmaz <h98579694@gmail.com>
2026-08-10 19:37:00 +0100

    Update help message in KfcHelpCommand
    woo

--- src/main/java/eu/koolfreedom/command/impl/KfcHelpCommand.java
+++ src/main/java/eu/koolfreedom/command/impl/KfcHelpCommand.java
@@ -12,8 +12,13 @@ public class KfcHelpCommand extends KoolCommand {
 
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args) {
+        msg(sender, "<dark_gray>--------------------------------");
+        msg(sender, "<gold><Bold>KoolSmpCore Help"
         msg(sender, "<red>If you need help setting up KoolSmpCore, please visit our discord here: https://discord.gg/MTYrSgVkmd");
-        msg(sender, "<red>Or run /help KoolSmpCore");
+        msg(sender, "<red>Or run /help KoolSmpCore for help with commands.");
+        msg(sender, "<red>You can also configure stuff in the plugins config.yml folder.");
+        msg(sender, "<red>This can be found in<gold> plugins/koolsmpcore/config.yml");
+        msg(sender, "<dark_gray>--------------------------------");
         
         return true;
     }
